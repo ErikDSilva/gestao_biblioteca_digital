8d7072df5958e321a0be86dc1adbb99eca43342a
KayoRonald <kayoronald4321@gmail.com>
2025-07-09 10:39:14 -0300

    Desserializa a lista de livros ao chamar a classe

--- src/main/java/br/edu/ifpb/gestaobibliotecadigital/controllers/LivroController.java
+++ src/main/java/br/edu/ifpb/gestaobibliotecadigital/controllers/LivroController.java
@@ -1,5 +1,10 @@
 package br.edu.ifpb.gestaobibliotecadigital.controllers;
 
+import br.edu.ifpb.gestaobibliotecadigital.services.impl.LivroService;
+
 public class LivroController {
+
+    private LivroService service = new LivroService();
+    
     
 }
