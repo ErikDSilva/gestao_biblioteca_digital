018ccb1594477703c66a46fbf8d475e62276dc2c
KayoRonald <kayoronald4321@gmail.com>
2025-07-09 11:23:08 -0300

    Coleção

--- src/main/java/br/edu/ifpb/gestaobibliotecadigital/models/livros/Colecao.java
+++ src/main/java/br/edu/ifpb/gestaobibliotecadigital/models/livros/Colecao.java
@@ -1,5 +1,7 @@
 package br.edu.ifpb.gestaobibliotecadigital.models.livros;
 
-public class Colecao {
+import java.io.Serializable;
+
+public class Colecao extends Livro implements Serializable {
     
 }
