d0feec176d3f6de0e3659634a78266a0532b97ab
KayoRonald <kayoronald4321@gmail.com>
2025-07-09 11:39:06 -0300

    Implementando a interface ILivroService

--- src/main/java/br/edu/ifpb/gestaobibliotecadigital/services/impl/LivroService.java
+++ src/main/java/br/edu/ifpb/gestaobibliotecadigital/services/impl/LivroService.java
@@ -1,5 +1,34 @@
 package br.edu.ifpb.gestaobibliotecadigital.services.impl;
 
-public class LivroService {
+import br.edu.ifpb.gestaobibliotecadigital.models.livros.Livro;
+import br.edu.ifpb.gestaobibliotecadigital.repositories.LivroRepository;
+import br.edu.ifpb.gestaobibliotecadigital.services.interfaces.ILivroService;
+import java.util.List;
+
+public class LivroService implements ILivroService {
+
+    private final LivroRepository livroRepository = new LivroRepository();
+
+    @Override
+    public Livro adicionarLivro(String titulo, String autor, int ano, String editora, String isbn, String sinopse, String categoria) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+    }
+
+    @Override
+    public Livro buscarLivroPorIsbn(String isbn) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Livro> listarTodosLivros() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void removerLivro(String isbn) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+  
     
 }
