c27419d5ddc38a822f5b1822305fd0fca84427cf
marcostsi <lira.santos@academico.ifpb.edu.br>
2025-07-11 12:52:34 -0300

    Estratégias de empréstimo

--- src/main/java/br/edu/ifpb/gestaobibliotecadigital/emprestimos/estrategias/EmprestimoPadrao.java
+++ src/main/java/br/edu/ifpb/gestaobibliotecadigital/emprestimos/estrategias/EmprestimoPadrao.java
@@ -2,4 +2,24 @@ package br.edu.ifpb.gestaobibliotecadigital.emprestimos.estrategias;
 
 public class EmprestimoPadrao implements EstrategiaEmprestimo {
 
+    @Override
+    public String getNome() {
+        return "Empréstimo Padrão";
+    }
+
+    @Override
+    public int calcularMaximoDias() {
+        return 7;
+    }
+
+    @Override
+    public double calcularMulta(int diasAtraso) {
+        return diasAtraso * 1.00;
+    }
+
+    @Override
+    public int calcularMaximoRenovacoes() {
+        return 2;
+    }
+
 }
--- src/main/java/br/edu/ifpb/gestaobibliotecadigital/emprestimos/estrategias/EstrategiaEmprestimo.java
+++ src/main/java/br/edu/ifpb/gestaobibliotecadigital/emprestimos/estrategias/EstrategiaEmprestimo.java
@@ -4,4 +4,12 @@ import java.io.Serializable;
 
 public interface EstrategiaEmprestimo extends Serializable {
 
+    public String getNome();
+
+    public int calcularMaximoDias();
+
+    public double calcularMulta(int diasAtraso);
+
+    public int calcularMaximoRenovacoes();
+
 }
