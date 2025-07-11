c772c7113228796a90869d4d83d753a981066a7c
KayoRonald <kayoronald4321@gmail.com>
2025-07-11 17:43:14 -0300

    Ajeitando lógica de persistência de livros no repositório e no serviço
    Co-authored-by: Marcelo Alves <marcelo-oliveira.mo@academico.ifpb.edu.br>

--- src/main/java/br/edu/ifpb/gestaobibliotecadigital/services/impl/LivroService.java
+++ src/main/java/br/edu/ifpb/gestaobibliotecadigital/services/impl/LivroService.java
@@ -7,39 +7,4 @@ import java.util.List;
 
 public class LivroService implements ILivroService {
 
-    private final LivroRepository livroRepository = new LivroRepository();
-
-    public LivroService() {
-        livroRepository.deserializarLivros();
-    }
-
-    @Override
-    public Livro adicionarLivro(Livro livro) {
-        livroRepository.livros.add(livro);
-        livroRepository.serializarLivros();
-        return livro;
-    }
-
-    @Override
-    public Livro buscarLivroPorIsbn(String isbn) {
-        for (Livro livro : livroRepository.livros) {
-            if (livro.getIsbn().equals(isbn)) {
-                return livro;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public List<Livro> listarTodosLivros() {
-        return livroRepository.livros;
-    }
-
-    @Override
-    public void removerLivro(String isbn) {
-        Livro livro = buscarLivroPorIsbn(isbn);
-        livroRepository.livros.remove(livro);
-        livroRepository.serializarLivros();
-    }
-
 }
