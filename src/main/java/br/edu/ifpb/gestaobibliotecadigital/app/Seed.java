482577468166f7dc04531870ddc87c2637c8df4f
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2025-07-15 20:52:37 -0300

    Corrigido erros ao dar merge com equipe 2

--- src/main/java/br/edu/ifpb/gestaobibliotecadigital/app/Seed.java
+++ src/main/java/br/edu/ifpb/gestaobibliotecadigital/app/Seed.java
@@ -19,7 +19,7 @@ public class Seed {
 
     private static final EmprestimoRepository emprestimoRepository = EmprestimoRepository.getInstance();
     private static final ReservaRepository reservaRepository = ReservaRepository.getInstance();
-    private static final LivroRepository livroRepository = new LivroRepository();
+    private static final LivroRepository livroRepository = LivroRepository.getInstance();
     private static final HistoricoRepository historicoRepository = HistoricoRepository.getInstance();
 
     public static void main(String[] args) {
@@ -28,20 +28,20 @@ public class Seed {
 
     public static void executarSeed() {
         System.out.println("[SEED] Limpando dados antigos...");
-        // livroRepository.resetar();
         // usuarioRepository.resetar();
+        livroRepository.resetar();
         emprestimoRepository.resetar();
         historicoRepository.resetar();
         reservaRepository.resetar();
 
         System.out.println("[SEED] Criando livros...");
-        
+
         Livro padroeDeProjeto = new LivroBuilder()
                 .setTitulo("Padrões de Projetos: Soluções Reutilizáveis de Software Orientados a Objetos")
                 .setAutor("Erich Gamma")
                 .setAno(2000)
                 .setEditora("Bookman")
-                .setIsbn("978-8573076103")
+                .setISBN("978-8573076103")
                 .setSinopse("Catálogo de soluções simples e sucintas para os problemas mais frequentes na área de projeto.")
                 .setCategoria("Tecnologia")
                 .builder();
@@ -51,7 +51,7 @@ public class Seed {
                 .setAutor("Kathy Sierra")
                 .setAno(2024)
                 .setEditora("Alta Books")
-                .setIsbn("978-8550819884")
+                .setISBN("978-8550819884")
                 .setSinopse("Experiência completa de aprendizado em Java e programação orientada a objetos.")
                 .setCategoria("Tecnologia")
                 .builder();
@@ -61,7 +61,7 @@ public class Seed {
                 .setAutor("Kathy Sierra")
                 .setAno(2017)
                 .setEditora("Novatec")
-                .setIsbn("978-8575225639")
+                .setISBN("978-8575225639")
                 .setSinopse("Introdução prática aos algoritmos mais utilizados.")
                 .setCategoria("Tecnologia")
                 .builder();
@@ -71,7 +71,7 @@ public class Seed {
                 .setAutor("J.R.R. Tolkien")
                 .setAno(2019)
                 .setEditora("HarperCollins")
-                .setIsbn("978-8595084759")
+                .setISBN("978-8595084759")
                 .setSinopse("Bilbo deixa o Condado para Frodo iniciar uma jornada pela Terra-média.")
                 .setCategoria("Fantasia")
                 .builder();
@@ -81,7 +81,7 @@ public class Seed {
                 .setAutor("J.R.R. Tolkien")
                 .setAno(2019)
                 .setEditora("HarperCollins")
-                .setIsbn("978-8595084766")
+                .setISBN("978-8595084766")
                 .setSinopse("A jornada continua com Legolas, Gimli e Aragorn tentando resgatar os hobbits.")
                 .setCategoria("Fantasia")
                 .builder();
@@ -91,7 +91,7 @@ public class Seed {
                 .setAutor("J.R.R. Tolkien")
                 .setAno(2019)
                 .setEditora("HarperCollins")
-                .setIsbn("978-8595084773")
+                .setISBN("978-8595084773")
                 .setSinopse("Sam e Frodo enfrentam o desafio final em Mordor.")
                 .setCategoria("Fantasia")
                 .builder();
@@ -101,7 +101,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2008)
                 .setEditora("VR Editora")
-                .setIsbn("978-8576831303")
+                .setISBN("978-8576831303")
                 .setSinopse("Não é fácil ser criança. E ninguém sabe disso melhor do que Greg Heffley, que se vê mergulhado no mundo do ensino fundamental.")
                 .setCategoria("Infantil")
                 .builder();
@@ -111,7 +111,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2009)
                 .setEditora("VR Editora")
-                .setIsbn("978-8576831952")
+                .setISBN("978-8576831952")
                 .setSinopse("Faça o que quiser, só não pergunte a Greg Heffley como foram suas férias de verão, porque ele realmente não quer falar sobre isso.")
                 .setCategoria("Infantil")
                 .builder();
@@ -121,7 +121,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2010)
                 .setEditora("VR Editora")
-                .setIsbn("978-8576832294")
+                .setISBN("978-8576832294")
                 .setSinopse("Greg não toma jeito mesmo. E a cada dia se envolve em mais confusão. O difícil é fazer seu pai engolir esse talento de Greg para se meter em situações embaraçosas.")
                 .setCategoria("Infantil")
                 .builder();
@@ -131,7 +131,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2011)
                 .setEditora("Vergara e Riba")
-                .setIsbn("978-8576832768")
+                .setISBN("978-8576832768")
                 .setSinopse("Férias de verão: o tempo está lindo, e toda garotada está se divertindo ao ar livre. Onde está Greg Heffley?")
                 .setCategoria("Infantil")
                 .builder();
@@ -141,7 +141,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2011)
                 .setEditora("VR Editora")
-                .setIsbn("978-8576832768")
+                .setISBN("978-8576832768")
                 .setSinopse("Greg Heffley sem quis crescer logo. Mas será que ficar mais velho é tão legal assim? De repente, Greg começa a lidar com as pressões das festas de meninos e meninas.")
                 .setCategoria("Infantil")
                 .builder();
@@ -151,7 +151,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2011)
                 .setEditora("VR Editora")
-                .setIsbn("978-8576833680")
+                .setISBN("978-8576833680")
                 .setSinopse("Em sua sexta aventura, Greg Heffley passará por grandes apuros. As coisas para ele vão de mal a pior, tanto na escola como em sua casa. Greg será suspeito de vandalismo e se tornará um suposto foragido da polícia.")
                 .setCategoria("Infantil")
                 .builder();
@@ -161,7 +161,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2013)
                 .setEditora("VR Editora")
-                .setIsbn("978-8576834847")
+                .setISBN("978-8576834847")
                 .setSinopse("O Dia dos Namorados está chegando e Greg Heffley continua sozinho. Mas um baile organizado pela escola pode mudar tudo. Ele precisa encontrar uma garota urgente. Para isso, conta com a ajuda de outro solteirão, Rowley, seu melhor amigo.")
                 .setCategoria("Infantil")
                 .builder();
@@ -171,7 +171,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2014)
                 .setEditora("VR Editora")
-                .setIsbn("978-8576836902")
+                .setISBN("978-8576836902")
                 .setSinopse("Greg Heffley está bolado. Seu companheiro de todas as horas, Rowley, o abandonou, e encontrar novos amigos na escola acabou se revelando uma tarefa muito difícil.")
                 .setCategoria("Infantil")
                 .builder();
@@ -181,7 +181,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2015)
                 .setEditora("VR Editora")
-                .setIsbn("978-8576838234")
+                .setISBN("978-8576838234")
                 .setSinopse("As férias do Greg tinham tudo para serem perfeitas, até que sua mãe vem com a bomba: eles farão uma viagem de carro em família. ")
                 .setCategoria("Infantil")
                 .builder();
@@ -191,7 +191,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2015)
                 .setEditora("VR Editora")
-                .setIsbn("978-8576839422")
+                .setISBN("978-8576839422")
                 .setSinopse("Greg Heffley está prestes a descobrir que a vida era muito melhor nos velhos tempos. Com sua cidade voluntariamente 'desconectada', a vida moderna tem seus desafios.")
                 .setCategoria("Infantil")
                 .builder();
@@ -201,7 +201,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2016)
                 .setEditora("VR Editora")
-                .setIsbn("978-8550700601")
+                .setISBN("978-8550700601")
                 .setSinopse("A pressão só aumenta para Greg Heffley. A única coisa em que ele parece ser realmente bom é em videojogos, mas a mãe quer que ele alargue os seus horizontes e faça mais alguma coisa - QUALQUER coisa! Quando o Greg encontra uma velha câmara de filmar, ele tem a certeza de que descobriu a forma ideal de, finalmente, mostrar a todos os seus grandes talentos escondidos.")
                 .setCategoria("Infantil")
                 .builder();
@@ -211,7 +211,7 @@ public class Seed {
                 .setAutor("Jeff Kinney")
                 .setAno(2017)
                 .setEditora("VR Editora")
-                .setIsbn("978-8550701448")
+                .setISBN("978-8550701448")
                 .setSinopse("Os Heffley estão dando o fora! Greg e sua família decidem fugir do frio e do estresse das festas de fim de ano e embarcam no primeiro avião com destino a uma ilha tropical. Mas, e se o paraíso não for tudo isso?")
                 .setCategoria("Infantil")
                 .builder();
@@ -222,7 +222,7 @@ public class Seed {
                 .setAutor("Júlio Verne")
                 .setAno(2023)
                 .setEditora("Pé da Letra")
-                .setIsbn("978-8595201736")
+                .setISBN("978-8595201736")
                 .setSinopse("Axel é um jovem órfão que mora em Hamburgo com seu tio Otto Lindenbrock, um renomado cientista da área de Geologia e Mineralogia. Um dia, o cientista encontra um pergaminho escrito em linguagem misteriosa.")
                 .setCategoria("Infantil")
                 .builder();
@@ -232,7 +232,7 @@ public class Seed {
                 .setAutor("Júlio Verne")
                 .setAno(2021)
                 .setEditora("Principis")
-                .setIsbn("978-8595201736")
+                .setISBN("978-8595201736")
                 .setSinopse("Depois de sequestrar um balão de um campo confederado, um grupo de cinco abolicionistas americanos cai das nuvens em uma ilha vulcânica desconhecida no oceano Pacífico.")
                 .setCategoria("Infantil")
                 .builder();
@@ -243,7 +243,7 @@ public class Seed {
                 .setAutor("Art Spiegelman")
                 .setAno(2005)
                 .setEditora("Quadrinhos na Cia")
-                .setIsbn("978-8535906288")
+                .setISBN("978-8535906288")
                 .setSinopse("Maus (\"rato\", em alemão) é a história de Vladek Spiegelman, judeu polonês que sobreviveu ao campo de concentração de Auschwitz, narrada por ele próprio ao filho Art.")
                 .setCategoria("Aventura")
                 .builder();
@@ -253,7 +253,7 @@ public class Seed {
                 .setAutor("George Orwell")
                 .setAno(2007)
                 .setEditora("Companhia das Letras")
-                .setIsbn("978-8535909555")
+                .setISBN("978-8535909555")
                 .setSinopse("Narra a insurreição dos animais de uma granja contra seus donos. Progressivamente, porém, a revolução degenera numa tirania ainda mais opressiva que a dos humanos.")
                 .setCategoria("Clássicos de Ficção")
                 .builder();
@@ -263,7 +263,7 @@ public class Seed {
                 .setAutor("Graciliano Ramos")
                 .setAno(2024)
                 .setEditora("Editora Itatiaia")
-                .setIsbn("978-6554700276")
+                .setISBN("978-6554700276")
                 .setSinopse("\"Vidas Secas\" é um retrato magistral das condições humanas diante das adversidades, explorando temas universais como a busca por um lar, a esperança em meio à desolação e a resistência humana perante a natureza implacável.")
                 .setCategoria("Clássicos de Ficção")
                 .builder();
@@ -273,7 +273,7 @@ public class Seed {
                 .setAutor("Holly Black")
                 .setAno(2004)
                 .setEditora("Rocco")
-                .setIsbn("978-8532517708")
+                .setISBN("978-8532517708")
                 .setSinopse("Fadas, duendes, gnomos e seres fantásticos, podem estar do seu lado agora mesmo, então fique alerta! Há muito mais nesse mundo do que os olhos podem ver. Para divulgar essa informação, os irmãos Grace pediram que Holly Black e Tony DiTerlizzi transformassem a sua história em uma série de cinco livros.")
                 .setCategoria("Infantil")
                 .builder();
@@ -283,7 +283,7 @@ public class Seed {
                 .setAutor("Holly Black")
                 .setAno(2004)
                 .setEditora("Rocco")
-                .setIsbn("978-8532517715")
+                .setISBN("978-8532517715")
                 .setSinopse("Jared e Mallory precisam salvar o irmão e ao mesmo tempo driblar todos os tipos de criaturas amedrontadoras. Eles também acabam fazendo estranhas alianças e conhecendo surpreendentes formas de vida.")
                 .setCategoria("Infantil")
                 .builder();
@@ -293,7 +293,7 @@ public class Seed {
                 .setAutor("Holly Black")
                 .setAno(2005)
                 .setEditora("Rocco")
-                .setIsbn("978-8532518798")
+                .setISBN("978-8532518798")
                 .setSinopse("Jared e Mallory precisam salvar o irmão e ao mesmo tempo driblar todos os tipos de criaturas amedrontadoras. Eles também acabam fazendo estranhas alianças e conhecendo surpreendentes formas de vida.")
                 .setCategoria("Infantil")
                 .builder();
@@ -303,7 +303,7 @@ public class Seed {
                 .setAutor("Holly Black")
                 .setAno(2005)
                 .setEditora("Rocco")
-                .setIsbn("978-8532518804")
+                .setISBN("978-8532518804")
                 .setSinopse("Jared está vendo dois Simon ao mesmo tempo; enquanto um remexe a bolsa de Mallory, o outro está sentado ao lado da mãe. Para piorar a situação, eles ainda têm pela frente a arriscada missão de resgatar a irmã das garras de Korting, Mulgarath e de todos os outros seres fantásticos que exigem o Guia de Campo como recompensa.")
                 .setCategoria("Infantil")
                 .builder();
@@ -313,7 +313,7 @@ public class Seed {
                 .setAutor("Holly Black")
                 .setAno(2006)
                 .setEditora("Rocco")
-                .setIsbn("978-8532519993")
+                .setISBN("978-8532519993")
                 .setSinopse("Mulgarath roubou o livro mágico de Arthur Spiderwick e deseja tornar-se o mestre do mundo. Com a ajuda do gnomo Tibério, do desajeitado Gritalhão e outros seres encantados que povoam a história, conseguirão Jared, Simon e Mallory triunfar sobre as forças do mal e fazer com que o precioso guia de campo seja usado para o bem de todos?")
                 .setCategoria("Infantil")
                 .builder();
@@ -355,11 +355,10 @@ public class Seed {
         ////////////////////////////////////////////////////////////////////////
         // TODO: adicionar coleções
         System.out.println("[SEED] Criando coleções de livros...");
-        
+
         ////////////////////////////////////////////////////////////////////////
-        
         System.out.println("[SEED] Criando usuários...");
-        
+
         Administrador adminCarlos = new Administrador("Carlos Silva", UUID.randomUUID().toString());
         Administrador adminFernanda = new Administrador("Fernanda Souza", UUID.randomUUID().toString());
 
@@ -386,9 +385,8 @@ public class Seed {
 //        usuarioRepository.adicionar(leitorRafael);
 //        usuarioRepository.adicionar(leitorJuliana);
         ////////////////////////////////////////////////////////////////////////
-        
         System.out.println("[SEED] Criando empréstimos...");
-        
+
         DataProvider.setDateTime("2025-01-01T00:00:00");
         var emp1 = new Emprestimo(leitorAna, useCabecaJava, new EmprestimoPadrao());
         DataProvider.setDateTime("2025-01-03T00:00:00");
@@ -420,7 +418,7 @@ public class Seed {
 
         ////////////////////////////////////////////////////////////////////////
         DataProvider.resetClock();
-        
+
         System.out.println("[SEED] Concluído");
     }
 }
