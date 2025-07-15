b4c9636a1bd3fb2156de3ae4226d1210739ddfc5
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2025-07-15 20:20:50 -0300

    Adicionado mais filtros ao Empréstimo

--- src/main/java/br/edu/ifpb/gestaobibliotecadigital/filters/EmprestimoFiltro.java
+++ src/main/java/br/edu/ifpb/gestaobibliotecadigital/filters/EmprestimoFiltro.java
@@ -2,9 +2,14 @@ package br.edu.ifpb.gestaobibliotecadigital.filters;
 
 import br.edu.ifpb.gestaobibliotecadigital.models.emprestimos.Emprestimo;
 import br.edu.ifpb.gestaobibliotecadigital.models.livros.Livro;
+import br.edu.ifpb.gestaobibliotecadigital.utils.Helpers;
 import java.util.List;
 
 public class EmprestimoFiltro extends Filtro<Emprestimo> {
+
+    public EmprestimoFiltro() {
+        super();
+    }
 
     public EmprestimoFiltro(List<Emprestimo> emprestimos) {
         super(emprestimos);
@@ -12,6 +17,26 @@ public class EmprestimoFiltro extends Filtro<Emprestimo> {
 
     public EmprestimoFiltro porLivro(Livro livro) {
         filtros.add(emprestimo -> emprestimo.getLivro().getId() == livro.getId());
+        return this;
+    }
+
+    public EmprestimoFiltro porNomeUsuario(String nome) {
+        filtros.add(emprestimo -> Helpers.normalizarTexto(emprestimo.getUsuario().getNome()).contains(Helpers.normalizarTexto(nome)));
+        return this;
+    }
+
+    public EmprestimoFiltro porNomeLivro(String nome) {
+        filtros.add(emprestimo -> Helpers.normalizarTexto(emprestimo.getLivro().getTitulo()).contains(Helpers.normalizarTexto(nome)));
+        return this;
+    }
+
+    public EmprestimoFiltro pesquisar(String texto) {
+        filtros.add(emprestimo -> {
+            String textoNormal = Helpers.normalizarTexto(texto);
+            String titulo = Helpers.normalizarTexto(emprestimo.getLivro().getTitulo());
+            String nomeUsuario = Helpers.normalizarTexto(emprestimo.getUsuario().getNome().toLowerCase());
+            return titulo.contains(textoNormal) || nomeUsuario.contains(textoNormal);
+        });
         return this;
     }
 
