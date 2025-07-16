22a01261c7ea92b9c2bc0a0730e3e18920593115
marcostsi <lira.santos@academico.ifpb.edu.br>
2025-07-16 08:38:30 -0300

    verifica o usuario logado

--- src/main/java/br/edu/ifpb/gestaobibliotecadigital/controllers/EmprestimoController.java
+++ src/main/java/br/edu/ifpb/gestaobibliotecadigital/controllers/EmprestimoController.java
@@ -6,31 +6,52 @@ import br.edu.ifpb.gestaobibliotecadigital.models.livros.Livro;
 import br.edu.ifpb.gestaobibliotecadigital.models.usuarios.Usuario;
 import br.edu.ifpb.gestaobibliotecadigital.services.impl.EmprestimoService;
 
-public class EmprestimoController {
+public class EmprestimoController extends Controller {
 
     private final EmprestimoService emprestimoService = new EmprestimoService();
 
     public void realizarEmprestimo(Usuario usuario, Livro livro) {
+        verificaUsuarioLogado();
+        verificaAdministrador();
         emprestimoService.solicitarEmprestimo(usuario, livro);
     }
 
     public void renovarEmprestimo(Emprestimo emprestimo) {
+        verificaUsuarioLogado();
+
+        // Verifica se é o proprio usuario
+        if (!isAdmin() && !usuarioLogado.getId().equals(emprestimo.getUsuario().getId())) {
+            throw new IllegalStateException("Você não pode renovar o empréstimo de outra pessoa");
+        }
+
         emprestimoService.renovarEmprestimo(emprestimo);
     }
 
     public void devolverLivro(Emprestimo emprestimo) {
+        verificaUsuarioLogado();
+        verificaAdministrador();
         emprestimoService.devolverLivro(emprestimo);
     }
 
     public void reservarLivro(Livro livro) {
+        verificaUsuarioLogado();
         emprestimoService.reservarLivro(usuarioLogado, livro);
     }
 
     public void reservarLivro(Usuario usuario, Livro livro) {
+        verificaUsuarioLogado();
+        verificaAdministrador();
         emprestimoService.reservarLivro(usuario, livro);
     }
 
     public void cancelarReserva(Reserva reserva) {
+        verificaUsuarioLogado();
+
+        // Verifica se é o proprio usuario
+        if (!isAdmin() && !usuarioLogado.getId().equals(reserva.getUsuario().getId())) {
+            throw new IllegalStateException("Você não pode cancelar a reserva de outra pessoa");
+        }
+
         emprestimoService.cancelarReserva(reserva);
     }
 }
